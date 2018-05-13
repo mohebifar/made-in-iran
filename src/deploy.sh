209a1589180cb44bb9e79b00bc904f41cf178327
Mohama Mohebifar <netfars@gmail.com>
2018-05-13 11:11:17 -0230

    chore(deploy): Update git author info

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,14 +1,7 @@
-yarn run build
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+git config --global user.email "deploy@travis-ci.org"
+git config --global user.name "Deployment Bot (from Travis CI)"
 git status
 git add .
-git status
-git branch
 git checkout master
-git branch
 git commit -m "Automatic update of README.md"
-git status
-git branch
-echo "Test value: $TEST"
 git push https://$GITHUB_AUTH_TOKEN@github.com/mohebifar/made-in-iran master
